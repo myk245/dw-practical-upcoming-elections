c685cca5b1a65bf163f57e29d3b5e6ec1c887e89
myk245 <myk245@nyu.edu>
2021-10-09 22:10:25 -0400

    add setUrl helper function

--- javascript/routes/index.js
+++ javascript/routes/index.js
@@ -2,6 +2,16 @@ var express = require('express');
 var router = express.Router();
 var postalAbbreviations = require('../us_state.js');
 
+// helper functions 
+const setUrl = (city, state) => {
+  // example url: https://api.turbovote.org/elections/upcoming?district-divisions=ocd-division/country:us/state:ma,ocd-division/country:us/state:ma/place:wayland
+  let state = state.toLowerCase();
+  let city = city.toLowerCase();
+
+  let url = `https://api.turbovote.org/elections/upcoming?district-divisions=ocd-division/country:us/state:${state},ocd-division/country:us/state:${state}/place:${city}`;
+
+}
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Find My Election', states: postalAbbreviations });
