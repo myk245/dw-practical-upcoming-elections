f82379e8aee7adcb9accd90cb9708428dfb1a895
myk245 <myk245@nyu.edu>
2021-10-09 22:20:50 -0400

    add notes to router

--- javascript/app.js
+++ javascript/app.js
@@ -18,6 +18,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// direct app to use indexRouter for all paths
 app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
--- javascript/routes/index.js
+++ javascript/routes/index.js
@@ -6,7 +6,8 @@ var postalAbbreviations = require('../us_state.js');
 const setUrl = (city, state) => {
   // example url: https://api.turbovote.org/elections/upcoming?district-divisions=ocd-division/country:us/state:ma,ocd-division/country:us/state:ma/place:wayland
   let state = state.toLowerCase();
-  let city = city.toLowerCase();
+  // remove spaces from city name
+  let city = city.toLowerCase().replace(/\s+/g, '');
 
   let url = `https://api.turbovote.org/elections/upcoming?district-divisions=ocd-division/country:us/state:${state},ocd-division/country:us/state:${state}/place:${city}`;
 
@@ -16,5 +17,7 @@ const setUrl = (city, state) => {
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Find My Election', states: postalAbbreviations });
 });
+
+
 
 module.exports = router;
